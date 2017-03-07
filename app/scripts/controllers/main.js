d175c1c54839e761000ed8a8061198d3ac4eacac
Javier <javimedina87@gmail.com>
2017-03-07 10:09:39 +0100

    Yeoman generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workyFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workyFrontApp
+ */
+angular.module('workyFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
